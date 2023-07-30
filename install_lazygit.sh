90c243717db5da2f73c6b65a5989a17dc0c3604c
DenZhigalov <denzhigalov@contorra.com>
2023-07-30 17:54:30 +0700

    Added ln line to lazygit installation

--- install_lazygit.sh
+++ install_lazygit.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
- 
+
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
@@ -7,3 +7,5 @@ sudo install lazygit /usr/local/bin
 
 rm lazygit
 rm lazygit.tar.gz
+
+ln -s $(which lazygit) ~/.local/bin/lg
