9083d4ae78fd6d7ac25582a1a456e2cfde941887
DenZhigalov <denzhigalov@contorra.com>
2023-08-03 16:18:23 +0700

    notifications to bottom

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,6 +2,7 @@ return {
   "rcarriga/nvim-notify",
   opts = {
     background_colour = "#000000",
+    top_down = false,
   },
   keys = {
     { "<leader>uN", ":Telescope notify<CR>", "Notify history" },
