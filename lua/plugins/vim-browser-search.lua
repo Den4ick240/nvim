74bd91dbbec35ed7e272dc2f2156cb647bd286ef
DenZhigalov <denzhigalov@contorra.com>
2023-08-03 16:18:52 +0700

    added vim-browser-search

---
+++ lua/plugins/vim-browser-search.lua
@@ -0,0 +1,7 @@
+return {
+  "voldikss/vim-browser-search",
+  keys = {
+    { "<leader>si", "<Plug>SearchNormal", desc = "Search internet", mode = "n" },
+    { "<leader>si", "<Plug>SearchVisual", desc = "Search internet", mode = "v" },
+  },
+}
